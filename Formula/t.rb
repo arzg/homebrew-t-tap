dbb2f49c50c4f6a3b7ebdeeef00739a663957be6
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-07 17:49:03 +1000

    Add initial formula

---
+++ Formula/t.rb
@@ -0,0 +1,11 @@
+class T < Formula
+    desc "Todo list in Rust"
+    homepage "https://github.com/arzg/t"
+    url "https://example.com"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    version "0.1.0"
+
+    def install
+      bin.install "t"
+    end
+  end
