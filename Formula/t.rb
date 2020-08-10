4157b3b519fef55e51676ba0c4eaa8de0292208d
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-10 18:05:11 +1000

    t 0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/t.rb
+++ Formula/t.rb
@@ -1,9 +1,9 @@
 class T < Formula
     desc "Todo list in Rust"
     homepage "https://github.com/arzg/t"
-    url "https://github.com/arzg/t/releases/download/v0.2.0/t.tar.gz"
-    sha256 "cc534c94ab6c595192b804b69ce515164f358bec759418c36eb977329de9f1e9"
-    version "0.2.0"
+    url "https://github.com/arzg/t/releases/download/v0.2.1/t.tar.gz"
+    sha256 "0b9a1de53e0422740febcde296e44d22c08d86bbc972d1e59abdfd1b0fcb1b6d"
+    version "0.2.1"
 
     def install
       bin.install "release/t"
